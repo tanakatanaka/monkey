99ca4f76d1830b61834c964f71367eb412660ccf
tanakatanaka <aiueo2003haru@yahoo.co.jp>
2013-10-01 09:01:47 +0000

    game.js: guiの位置調整

--- Assets/script/game.js
+++ Assets/script/game.js
@@ -44,22 +44,31 @@ function OnGUI()
 {
 	if(state == "GAME_SELECT")
 	{
-		var sw : int = Screen.width;
-		var sh : int = Screen.height;
-		var b_xy : int = sw / 10;
-		var place : int =(b_xy + b_xy /3);
-		var x : int = (sw / 2) - (place * 4) / 2;
-		var y : int = sh / 3;
+		var sw : float = Screen.width;
+		var sh : float = Screen.height;
+
+		// ボタンのサイズ
+		var size : float = sw / 10;
+		// ボタン間の間隔
+		var margin : float = size / 3;
+
+		var label_height : float = size / 4;
+
+		var total_ui_width : float = size * 5 + margin * 4;
+		var total_ui_height : float = label_height + margin + size;
+
+		var x : float = (sw - total_ui_width) / 2;
+		var y : float = (sh - total_ui_height) / 2;
 		
-		  
+		GUI.Box(new Rect(x, y, total_ui_width, label_height), "ゲームレベル");
+
+		y += label_height + margin;
 		
-		GUI.Box(new Rect(x,y / 2, x + place * 3, b_xy / 4), "ゲームレベル");
-		
-		if(GUI.Button(new Rect(x , y, b_xy, b_xy), "Level 1")) { this.game_level = 1; }
-		if(GUI.Button(new Rect(x + place, y, b_xy, b_xy), "Level 2")) { this.game_level = 2; }
-		if(GUI.Button(new Rect(x + place * 2, y, b_xy, b_xy), "Level 3")) { this.game_level = 3; }
-		if(GUI.Button(new Rect(x + place * 3, y, b_xy, b_xy), "Level 4")) { this.game_level = 4; }
-		if(GUI.Button(new Rect(x + place * 4, y, b_xy, b_xy), "Level 5")) { this.game_level = 5; }
+		if(GUI.Button(new Rect(x , y, size, size), "Level 1")) { this.game_level = 1; }
+		if(GUI.Button(new Rect(x + place, y, size, size), "Level 2")) { this.game_level = 2; }
+		if(GUI.Button(new Rect(x + place * 2, y, size, size), "Level 3")) { this.game_level = 3; }
+		if(GUI.Button(new Rect(x + place * 3, y, size, size), "Level 4")) { this.game_level = 4; }
+		if(GUI.Button(new Rect(x + place * 4, y, size, size), "Level 5")) { this.game_level = 5; }
 	
 		if(this.game_level > 0) { state = "GAME_PLAY"; }	
 	}
